56e9d888fcc423ccc8efb2aa07005921d5ea4b4f
rovicz <roviczzf@gmail.com>
2025-12-05 11:03:23 -0300

    feat: creating navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+export function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          CineWeb
+        </Link>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/filmes">
+                Filmes
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/salas">
+                Salas
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/sessoes">
+                Sessões
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
